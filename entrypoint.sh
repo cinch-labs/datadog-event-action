2cf8cd36660be5f5b99ac6874a08ce7353237c96
Apostolos-Daniel <dev@toli.io>
2020-08-03 09:47:40 +0100

    don't use sendEvent function

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,20 @@ fi
 echo "Send github action event to datadog"
 echo
 
-sendEvent() {
-    local messageTitle="${1}"
-    local message="${2}"
-    local tags="${3}"
-    local datadogUrl="https://api.datadoghq.eu/api/v1/events?api_key=${4}"
-    local alertType="${5}"
-    if [ -z "$alertType" ]; then
-        echo "No argument for alert type supplied - setting it to 'info'"
-        alertType="info"
-    fi
-    curl -X POST -H "Content-type: application/json" \
-        -d "{\"title\": \"${messageTitle}\",\"text\": \"${message}\",\"priority\": \"normal\",\"tags\": \"${tags}\",\"alert_type\": \"${alertType}\",\"source_type_name\": \"GITHUB\"}" \
-        "${datadogUrl}"
-}
+messageTitle="${1}"
+message="${2}"
+tags="${3}"
+datadogUrl="https://api.datadoghq.eu/api/v1/events?api_key=${4}"
+alertType="${5}"
+echo messageTitle
+echo message
+echo tags
+echo alertType
 
-sendEvent "${1}" "${2}" "${3}" "${4}" "${5}"
+if [ -z "$alertType" ]; then
+    echo "No argument for alert type supplied - setting it to 'info'"
+    alertType="info"
+fi
+curl -X POST -H "Content-type: application/json" \
+    -d "{\"title\": \"${messageTitle}\",\"text\": \"${message}\",\"priority\": \"normal\",\"tags\": \"${tags}\",\"alert_type\": \"${alertType}\",\"source_type_name\": \"GITHUB\"}" \
+    "${datadogUrl}"
