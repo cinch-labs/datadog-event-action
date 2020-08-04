8801aa09faca80cb9b93aec50eb242bfc9a0ec54
cinch-sarika <sarika.antony@cinch.co.uk>
2020-08-04 12:28:14 +0100

    moved -d to line above

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,9 +30,7 @@ if [ -z "$alertType" ]; then
     echo "No argument for alert type supplied - setting it to 'info'"
     alertType="info"
 fi
-curl -X POST -H "Content-type: application/json" \
-#-d "{\"title\": \"${messageTitle}\",\"text\": \"${message}\",\"priority\": \"normal\",\"tags\": \"${tags}\",\"alert_type\": \"${alertType}\",\"source_type_name\": \"GITHUB\"}" \
- -d '{
+curl -X POST -H "Content-type: application/json" -d '{
             "title": "'"${messageTitle}"'",
             "text": "'"${message}"'",
             "priority": "normal",
